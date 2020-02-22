4d8fe09c58942a0927da058ee7bf6f2b3be1ea95
MatheusAlves <m-alves7@hotmail.com>
2020-02-22 12:07:19 -0300

    Objeto game of life e Header

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,13 +1,32 @@
-import pygame
+# --------------------------- Conway's GameOfLife ---------------------------
+# Autor: MatheusAlves - Github: https://github.com/MatheusAQueiroz/GameOfLife
+# Variáveis de configuração
+(largura, altura) = 600, 600
+cor_ativa = (255,255,255)
+
+# ---------------------------------------------------------------------------
+
+# Bibliotecas
+import pygame  
+import numpy   
+
+# Classe com métodos relacionados ao jogo em si
+class gameOfLife:
+    # Método construtor
+    def __init__(self,tamanho_grade):
+        matriz = numpy.fill((self.tamanho_grade, self.tamanho_grade),True)
+    # 
+
 
 # Definição das coordenadas da tela
-(largura, altura) = 600, 600
 tela = pygame.display.set_mode((largura,altura))
 
-# Mantém a janela aberta
+# Mantém a janela aberta até o X ser apertado
 rodando = True
 while rodando:
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            rodando = False           
+            rodando = False
+
 
