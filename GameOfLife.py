2c2809b53ef6d2798115379ec01f10f8559ae1de
MatheusAlves <m-alves7@hotmail.com>
2020-02-22 16:45:04 -0300

    Calculo de quadro funcional

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,13 +1,14 @@
 # --------------------------- Conway's GameOfLife ---------------------------
 # Autor: MatheusAlves - Github: https://github.com/MatheusAQueiroz/GameOfLife
+
 # Variáveis de configuração
-
-cor_ativa = (255,255,255,255)
-tamanho_grade = 100
-tamanho_quadrado = 5
-grossura_grade = 1
+cor_ativa = (255,255,255)
+cor_inativa = (0,0,0)
+tamanho_grade = 10
+tamanho_quadrado = 15
+grossura_grade = 2
 margem = 3
-
+# Definição automática do tamanho da janela
 t = (tamanho_quadrado+grossura_grade)*tamanho_grade+2*margem
 (largura, altura) = t,t
 
@@ -15,7 +16,7 @@ t = (tamanho_quadrado+grossura_grade)*tamanho_grade+2*margem
 
 # Bibliotecas
 import pygame as pg
-import numpy  as np
+import numpy as np
 
 # Classe com propriedades e métodos relacionados ao jogo
 class gameOfLife:
@@ -24,14 +25,32 @@ class gameOfLife:
     # Método construtor
     def __init__(self):
         self.matriz = np.full((tamanho_grade, tamanho_grade),True)
-    # Método de desenhar a matriz na tela
+    # Método para desenhar a matriz na tela
     def dTela(self, surface):
+        surface.fill(cor_inativa)
         for lin in range(np.size(self.matriz,0)):
             for col in range(np.size(self.matriz,1)):
                 if self.matriz.item((lin,col)):
-                    pg.draw.rect(surface,cor_ativa,pg.Rect(margem+lin*(tamanho_quadrado+grossura_grade),margem+col*(tamanho_quadrado+grossura_grade),tamanho_quadrado,tamanho_quadrado),0)
+                    pg.draw.rect(surface,cor_ativa,pg.Rect(col*(tamanho_quadrado+grossura_grade)+margem,lin*(tamanho_quadrado+grossura_grade)+margem,tamanho_quadrado,tamanho_quadrado),0)
         pg.display.update()
-        
+    # Método para calcular o quadro seguinte
+    def calcQuadro(self):
+        matrizTemp = self.matriz.copy()
+        for lin in range(np.size(self.matriz,0)):
+            for col in range(np.size(self.matriz,1)):
+                # Contagem de vizinhos
+                c = 0
+                for x in range(-1,2):
+                    for y in range(-1,2):
+                        if (0<=lin+x<np.size(self.matriz,0)) and (0<=col+y<np.size(self.matriz,1)) and (not (x,y) == (0,0)) and self.matriz.item((lin+x,col+y)):
+                            c += 1
+                if self.matriz.item((lin,col)):
+                    if (c > 3 or c < 2):
+                        np.put(matrizTemp,col+(lin*tamanho_grade),False,mode='clip')
+                elif (c == 3):
+                    np.put(matrizTemp,matrizTemp,col+(lin*tamanho_grade),True)     
+        self.matriz = matrizTemp.copy()
+               
 # ---------------------------------------------------------------------------
 
 # Definição da pygame.Surface (Janela do windows)
@@ -41,7 +60,9 @@ tela = pg.display.set_mode((largura,altura))
 
 # Instanciação
 jogo = gameOfLife()
+jogo.dTela(tela)
 
+jogo.calcQuadro()
 jogo.dTela(tela)
 
 # ---------------------------------------------------------------------------
